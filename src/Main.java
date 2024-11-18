2d70827e7e53c90b15b5765537d11bd5d6baa7d2
KateShkuryna <shkurinakate@gmail.com>
2024-11-18 20:34:24 +0200

    Add method to calculate sum of two numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub Desktop!");
+
+        int sum = add(6, 4);
+        System.out.println("6 + 4 = " + sum);
+    }
+
+    public static int add(int a, int b){
+        return a + b;
     }
 }
